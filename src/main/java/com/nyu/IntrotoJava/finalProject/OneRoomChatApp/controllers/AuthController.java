df332eea6454e8d2b464d64f1684f1e4dc6dde98
jLiucoder <55926282+jLiucoder@users.noreply.github.com>
2023-11-28 22:10:13 -0500

    added interceptor with jwt to implement auth, backend auth module fully done

--- src/main/java/com/nyu/IntrotoJava/finalProject/OneRoomChatApp/controllers/AuthController.java
+++ src/main/java/com/nyu/IntrotoJava/finalProject/OneRoomChatApp/controllers/AuthController.java
@@ -2,14 +2,17 @@ package com.nyu.IntrotoJava.finalProject.OneRoomChatApp.controllers;
 
 import com.nyu.IntrotoJava.finalProject.OneRoomChatApp.models.Users;
 import com.nyu.IntrotoJava.finalProject.OneRoomChatApp.services.UsersService;
+import com.nyu.IntrotoJava.finalProject.OneRoomChatApp.util.JwtUtils;
 import com.nyu.IntrotoJava.finalProject.OneRoomChatApp.util.Result;
-import com.nyu.IntrotoJava.finalProject.OneRoomChatApp.util.UserDtoMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -38,7 +41,14 @@ public class AuthController {
         Users u = usersService.login(user);
 
         if(u != null){
-            return Result.success(UserDtoMapper.toDTO(u));
+//            generate token for the user
+            Map<String, Object> claims = new HashMap<>();
+            claims.put("userId", u.getUserId());
+            claims.put("username", u.getUsername());
+            claims.put("fullName", u.getFullName());
+            String token = JwtUtils.generateToken(claims);
+
+            return Result.success(token);
         }
         return Result.error("login failed, wrong username or password");
     }
--- src/test/java/com/nyu/IntrotoJava/finalProject/OneRoomChatApp/OneRoomChatAppApplicationTests.java
+++ src/test/java/com/nyu/IntrotoJava/finalProject/OneRoomChatApp/OneRoomChatAppApplicationTests.java
@@ -1,13 +1,22 @@
 package com.nyu.IntrotoJava.finalProject.OneRoomChatApp;
 
+import com.nyu.IntrotoJava.finalProject.OneRoomChatApp.util.JwtUtils;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootTest
 class OneRoomChatAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
+
 
 }
